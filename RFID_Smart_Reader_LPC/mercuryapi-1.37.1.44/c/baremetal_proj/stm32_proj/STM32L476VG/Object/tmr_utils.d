9a43628087827990fe6bdd1c4134b1d87ab088f4
andrei.karolyi <andrei.karolyi@student.upt.ro>
2024-02-24 17:06:21 +0200

    Removed unused files from MercuryAPI
    Signed-off-by: andrei.karolyi <andrei.karolyi@student.upt.ro>

--- RFID_Smart_Reader_LPC/mercuryapi-1.37.1.44/c/baremetal_proj/stm32_proj/STM32L476VG/Object/tmr_utils.d
+++
@@ -1,7 +0,0 @@
-./object/tmr_utils.o: ..\..\..\src\api\tmr_utils.c \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
-  ..\..\..\src\api\osdep.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  ..\..\..\src\api\tmr_types.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
-  ..\..\..\src\api\tmr_utils.h
