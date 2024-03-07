199311613fe8e0d8fcf121f5fcdb1a8ba7ba72b8
Andrei Karolyi <andrei.karolyi@student.upt.ro>
2024-03-07 17:39:30 +0200

    RFID Manager added for Raspberry PI (untested)
    Signed-off-by: Andrei Karolyi <andrei.karolyi@student.upt.ro>

--- External_I2C_API/RFID/RFID_Manager.h
+++ External_I2C_API/RFID/RFID_Manager.h
@@ -1,8 +1,27 @@
 #ifndef __RFID_MANAGER_H
 #define __RFID_MANAGER_H
 
-void RFID_Manager(void);
+typedef enum RFID_command_t
+{
+    RFID_GET_ROUTE_STATUS,
+    RFID_PING,
+    RFID_INVALID /*More requests will be added when implemented*/
+   ,
+}RFID_command_t;
 
+typedef enum RFID_request_status_t
+{
+    RFID_REQUEST_OK,
+    RFID_REQUEST_PENDING,
+    RFID_REQUEST_NOK_INVALID_COMMAND,
+    RFID_REQUEST_NOK_INVALID_CRC,
+    RFID_REQUEST_NOK_TX_COMM_ERROR,
+    RFID_REQUEST_NOK_RX_COMM_ERROR
+}RFID_request_status_t;
 
+#define RFID_IS_VALID_DATA(data) ((uint8_t)data != (uint8_t)0xFFU)
+
+RFID_request_status_t RFID_init(void);
+RFID_request_status_t RFID_sendRequest(const RFID_command_t command, const uint8_t in_data, uint8_t* const out_data);
 
 #endif
